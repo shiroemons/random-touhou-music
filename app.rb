c042e4afc7fd3ce39692e77dabafdc485304e99a
shiroemons <shiroemons@gmail.com>
2021-01-24 13:03:27 +0900

    Added routing to show the number of songs
    - Add routing for '/apple_music'

--- app.rb
+++ app.rb
@@ -4,14 +4,22 @@ require 'sinatra'
 APPLE_MUSIC_SONGS = JSON.parse(File.read("data/apple_music_songs.json")).freeze
 YOUTUBE_MUSIC_SONGS = JSON.parse(File.read("data/youtube_music_songs.json")).freeze
 
-get '/' do
+get ['/', '/apple_music'] do
   song = APPLE_MUSIC_SONGS.sample
   logger.info song
   redirect song["url"]
+end
+
+get '/apple_music/songs_count' do
+  "#{APPLE_MUSIC_SONGS.size}曲"
 end
 
 get '/youtube_music' do
   song = YOUTUBE_MUSIC_SONGS.sample
   logger.info song
   redirect song["url"]
+end
+
+get '/youtube_music/songs_count' do
+  "#{YOUTUBE_MUSIC_SONGS.size}曲"
 end
